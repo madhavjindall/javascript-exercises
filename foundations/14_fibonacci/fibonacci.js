4f927db3cce3f72fbccb091c6a2b96bc1b74ec3f
Madhav Jindal <madhavjindal616@gmail.com>
2026-06-04 15:54:04 +0530

    sadasd

--- foundations/14_fibonacci/fibonacci.js
+++ foundations/14_fibonacci/fibonacci.js
@@ -1,5 +1,29 @@
-const fibonacci = function() {
+const fibonacci = function(a) {
+    
+    let first=1;
+    let second =1;
+    if(a<0){
+        return "OOPS";
+    }
+    if(a==0){
+        return 0;
+    }
+    if(a==1){
+        return first;
+    }
+    if(a==2){
+        return second;
+    }
+    let sum=0;
 
+    if(a>2){
+    for(let i=3;i<=a;i++){
+        sum=first+second;
+        first=second;
+        second =sum;
+        }
+    }      
+    return sum;
 };
 
 // Do not edit below this line
